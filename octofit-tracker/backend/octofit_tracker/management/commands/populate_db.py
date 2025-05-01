ef56e213e590c48286a4ccf7a3a839662b583051
Aline Chimchek <138634777+alinechimchek@users.noreply.github.com>
2025-05-01 00:40:25 +0000

    Initialize and create the octofit_db MongoDB database, Django project/app, update Django project/app files, and populate the MongoDB database

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,66 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from pymongo import MongoClient
+from bson import ObjectId
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data for users, teams, activity, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        client = MongoClient(settings.MONGODB_SETTINGS['HOST'], settings.MONGODB_SETTINGS['PORT'])
+        db = client[settings.MONGODB_SETTINGS['DB']]
+
+        # Drop existing collections
+        db.users.drop()
+        db.teams.drop()
+        db.activity.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
+
+        # Create users
+        users = [
+            {"_id": ObjectId(), "username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+            {"_id": ObjectId(), "username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+            {"_id": ObjectId(), "username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+            {"_id": ObjectId(), "username": "crashoverride", "email": "crashoverride@hmhigh.edu", "password": "crashoverridepassword"},
+            {"_id": ObjectId(), "username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+        ]
+        db.users.insert_many(users)
+
+        # Create teams
+        blue_team = {"_id": ObjectId(), "name": "Blue Team", "members": [u["_id"] for u in users]}
+        gold_team = {"_id": ObjectId(), "name": "Gold Team", "members": [u["_id"] for u in users]}
+        db.teams.insert_many([blue_team, gold_team])
+
+        # Create activities
+        activities = [
+            {"_id": ObjectId(), "user": users[0]["_id"], "activity_type": "Cycling", "duration": str(timedelta(hours=1))},
+            {"_id": ObjectId(), "user": users[1]["_id"], "activity_type": "Crossfit", "duration": str(timedelta(hours=2))},
+            {"_id": ObjectId(), "user": users[2]["_id"], "activity_type": "Running", "duration": str(timedelta(hours=1, minutes=30))},
+            {"_id": ObjectId(), "user": users[3]["_id"], "activity_type": "Strength", "duration": str(timedelta(minutes=30))},
+            {"_id": ObjectId(), "user": users[4]["_id"], "activity_type": "Swimming", "duration": str(timedelta(hours=1, minutes=15))},
+        ]
+        db.activity.insert_many(activities)
+
+        # Create leaderboard entries
+        leaderboard_entries = [
+            {"_id": ObjectId(), "user": users[0]["_id"], "score": 100},
+            {"_id": ObjectId(), "user": users[1]["_id"], "score": 90},
+            {"_id": ObjectId(), "user": users[2]["_id"], "score": 95},
+            {"_id": ObjectId(), "user": users[3]["_id"], "score": 85},
+            {"_id": ObjectId(), "user": users[4]["_id"], "score": 80},
+        ]
+        db.leaderboard.insert_many(leaderboard_entries)
+
+        # Create workouts
+        workouts = [
+            {"_id": ObjectId(), "name": "Cycling Training", "description": "Training for a road cycling event"},
+            {"_id": ObjectId(), "name": "Crossfit", "description": "Training for a crossfit competition"},
+            {"_id": ObjectId(), "name": "Running Training", "description": "Training for a marathon"},
+            {"_id": ObjectId(), "name": "Strength Training", "description": "Training for strength"},
+            {"_id": ObjectId(), "name": "Swimming Training", "description": "Training for a swimming competition"},
+        ]
+        db.workouts.insert_many(workouts)
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the octofit_db database with test data.'))
